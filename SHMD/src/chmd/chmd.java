d4faa3744f09ae554ca389a0078bf543fd9bafe6
SaidNM <saidnm044@hotmail.com>
2017-09-10 22:35:00 -0500

    echo datagrama

--- SHMD/src/chmd/chmd.java
+++ SHMD/src/chmd/chmd.java
@@ -7,19 +7,26 @@ public class chmd {
     public static void main(String[] args) {
         try{
             DatagramSocket cl = new DatagramSocket();
-            System.out.println("Escribe la direccion del servidor: ");
+            System.out.println("Escribe un mensaje: ");
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String host = br.readLine();
-            String saludo = "Hola servidor como estas";
+            String saludo =br.readLine();
             
-            byte[] b = saludo.getBytes();
-            DatagramPacket p = new DatagramPacket(b,b.length,InetAddress.getByName(host),5000);
-            cl.send(p);
+            byte[] bm = saludo.getBytes();
+            ByteArrayInputStream bais = new ByteArrayInputStream(bm);
             
-            DatagramPacket p1 = new DatagramPacket(new byte[1500],1500);
-            cl.receive(p1);
-            
-            System.out.println("Saludo desde: "+p1.getSocketAddress()+" pto: "+p1.getPort()+" mensaje: "+new String(p1.getData(),0,p1.getLength()));
+            if(bm.length>=10){
+                int n=0;
+                byte [] b = new byte[10];
+                while((n=bais.read(b))!=-1){ 
+                    DatagramPacket p = new DatagramPacket(b,n,InetAddress.getByName("localhost"),5000);
+                    cl.send(p);
+
+                    DatagramPacket p1 = new DatagramPacket(new byte[1500],1500);
+                    cl.receive(p1);
+                    
+                    System.out.println("Saludo desde: "+p1.getSocketAddress()+" pto: "+p1.getPort()+" mensaje: "+new String(p1.getData(),0,p1.getLength()));
+                }    
+            }
             br.close();
             cl.close();
         }catch(Exception e){
